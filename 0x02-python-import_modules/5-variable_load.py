e5c5ae5bd3cd065685e698591c78a6fc2d36f806
Chigozie-css <chigoziedivine62@gmail.com>
2023-11-30 11:44:22 -0800

    A program that imports the variable a from the file variable_load_5.py and prints its value.

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,4 +2,5 @@
 
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
-    print(__import__("variable_load_5").a)
+    from variable_load_5 import a
+    print(a)
