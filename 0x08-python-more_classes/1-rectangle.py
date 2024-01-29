a18dbd5c32455513c0b74765a7f3947048f3df14
Chigozie-css <chigoziedivine62@gmail.com>
2024-01-29 11:00:37 -0800

    Definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
+
 class Rectangle:
-    """Represents a rectangle"""
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle."""
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")"""
-        self._validate_integer_positive(value, "width")
-        self._width = value
+        """Set the width of the rectangle."""
+        self._validate_dimension(value, "width")
+        self.__width = value
 
     @property
     def height(self):
-        return self._height
+        """Get the height of the rectangle."""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")"""
-        self._validate_integer_positive(value, "height")
-        self._height = value
+        """Set the height of the rectangle."""
+        self._validate_dimension(value, "height")
+        self.__height = value
 
-    def _validate_integer_positive(self, value, name):
+    def _validate_dimension(self, value, name):
+        """Validate the dimension (width or height)."""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value < 0:
             raise ValueError(f"{name} must be >= 0")
 
     def __str__(self):
-        return f"{{'_Rectangle__width': {self._width}, '_Rectangle__height': {self._height}}}"
+        """Return a string representation of the rectangle."""
+        return f"{{'_Rectangle__height': {self.__height}, '_Rectangle__width': {self.__width}}}"
