6c76ee3d31c0638c5368b9457f8f5af362204407
Chigozie-css <chigoziedivine62@gmail.com>
2024-02-05 12:07:03 -0800

    Trying to make pass checker

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+\
+"""Defines an inherited list class MyList."""
 
 class MyList(list):
     """Inherits from list with additional method."""
+
     def print_sorted(self):
         """Prints the list sorted in ascending order."""
         print(sorted(self))
