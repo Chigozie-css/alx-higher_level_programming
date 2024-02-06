a68f3fcfc402cb6694cdcb673e2992fe83b80774
Chigozie-css <chigoziedivine62@gmail.com>
2024-02-06 08:01:57 -0800

    Save Object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,34 +1,8 @@
 #!/usr/bin/python3
+"""Defines a Json file write function"""
+import json
+
 def save_to_json_file(my_obj, filename):
-    """
-    Write an Object to a text file using a JSON representation.
-
-    Args:
-        my_obj: The object to write to the file.
-        filename (str): The name of the file to save the JSON representation to.
-    """
-    def to_json_string(my_obj):
-        """
-        Return the JSON representation of an object (string).
-
-        Args:
-            my_obj: The object to convert to JSON.
-
-        Returns:
-            str: The JSON representation of the object.
-        """
-        if isinstance(my_obj, (str, int, float)):
-            return '"' + str(my_obj).replace('"', r'\"') + '"'
-        elif isinstance(my_obj, bool):
-            return 'true' if my_obj else 'false'
-        elif my_obj is None:
-            return 'null'
-        elif isinstance(my_obj, (list, tuple)):
-            items = ', '.join(to_json_string(item) for item in my_obj)
-            return '[' + items + ']'
-        elif isinstance(my_obj, dict):
-            items = ', '.join(f'"{key}": {to_json_string(value)}' for key, value in my_obj.items())
-            return '{' + items + '}'
-
-    with open(filename, mode='w', encoding='utf-8') as f:
-        f.write(to_json_string(my_obj))
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
