1f30fb7bbe19034bb901717429b1acff45922a7e
Chigozie-css <chigoziedivine62@gmail.com>
2024-02-07 11:05:00 -0800

    Task 5 and 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Defines an empty class BaseGeometry."""
+
+class BaseGeometry:
+    """Represents a base geometry class."""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents a base geometry class."""
+
+    def area(self):
+        """Calculates the area of the geometry."""
+        raise Exception("area() is not implemented")
