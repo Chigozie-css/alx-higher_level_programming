14d762f0ec53198648ef706f1c3ed24d3aed14d4
Chigozie-css <chigoziedivine62@gmail.com>
2024-03-13 08:50:41 -0700

    Updated task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,11 @@
--- Check if the user exists
+-- Check if the user user_0d_1 exists
 SELECT User, Host FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost';
 
--- Check the grants for the user
+-- Check the grants for the user user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Check if the user user_0d_2 exists
+SELECT User, Host FROM mysql.user WHERE User = 'user_0d_2' AND Host = 'localhost';
+
+-- Check the grants for the user user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
