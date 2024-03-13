5f25c0ae963e2e858787208edfd041d5938b20f9
Chigozie-css <chigoziedivine62@gmail.com>
2024-03-13 08:47:30 -0700

    Updated task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
-i-- Script to create the table 'id_not_null' if it doesn't exist
+-- Script to create the table 'id_not_null' if it doesn't exist
 CREATE TABLE IF NOT EXISTS `id_not_null` (
-	`id`   INT NOT NULL DEFAULT 1,
-	`name` VARCHAR(256)
+        `id`   INT NOT NULL DEFAULT 1,
+        `name` VARCHAR(256)
 );
