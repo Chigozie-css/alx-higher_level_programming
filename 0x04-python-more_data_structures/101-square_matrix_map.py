bb8f73b01d1d575e95aed8805c4826bdf8930639
Chigozie-css <chigoziedivine62@gmail.com>
2024-03-15 15:10:58 -0700

    Sorting error

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    """
-    Square each element of a matrix using map function.
-
-    Args:
-        matrix (list of lists): The matrix to be squared.
-
-    Returns:
-        list of lists: The matrix with each element squared.
-    """
+def square_matrix_map(matrix=[]);
     return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
