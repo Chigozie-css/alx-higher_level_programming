bcf18c76ab6e724fa66f7172256bf0f5b3a7891c
Chigozie-css <chigoziedivine62@gmail.com>
2024-03-18 11:12:46 -0700

    Sorting task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,14 +11,17 @@ def list_division(my_list_1, my_list_2, list_length):
 
     Returns:
         A new list of length `list_length` containing all the divisions.
-        If any element in `my_list_2` is zero or the lists are of different lengths, 
+        If any element in `my_list_2` is zero or the lists are of different lengths,
         it returns 0 for the corresponding element in the result list.
         If any element in either list is not a number (e.g., string), it returns 0 for that element.
     """
     result_list = []
     for i in range(list_length):
         try:
-            div = my_list_1[i] / my_list_2[i]
+            if i < len(my_list_1) and i < len(my_list_2):
+                div = my_list_1[i] / my_list_2[i]
+            else:
+                raise IndexError
         except TypeError:
             print("wrong type")
             div = 0
@@ -32,14 +35,16 @@ def list_division(my_list_1, my_list_2, list_length):
             result_list.append(div)
     return result_list
 
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+# Test cases
+if __name__ == "__main__":
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
 
-print("--")
+    print("--")
 
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
