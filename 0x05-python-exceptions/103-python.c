2f41c2c1a5c9a7a94f0ad3ee5cefbe4d15486a70
Chigozie-css <chigoziedivine62@gmail.com>
2024-03-18 12:10:47 -0700

    Sorting bugs

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -65,6 +65,7 @@ void print_python_list(PyObject *p)
 		for (Py_ssize_t i = 0; i < size; ++i)
 		{
 			PyObject *item = PyList_GET_ITEM(p, i);
+
 			printf("Element %zd: %s\n", i, item->ob_type->tp_name);
 			if (PyBytes_Check(item))
 				print_python_bytes(item);
