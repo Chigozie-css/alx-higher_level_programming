b6664471dcf191f3764875e3bd89fe3785837fd5
Chigozie-css <chigoziedivine62@gmail.com>
2024-04-11 13:49:54 -0700

    Task 9 correction

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // This function adds two numbers and prints the result.
 function add(a, b) {
-  const c = a + b; // Calculate the sum of the two numbers
-  console.log(c); // Print the result to the console
+  const c = a + b;
+  console.log(c);
 }
 
-// Call the add function with the first and second command-line arguments as numbers
 add(Number(process.argv[2]), Number(process.argv[3]));
