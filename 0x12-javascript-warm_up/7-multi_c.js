d82a1043743b9ad91253854c96b4dee8ec9cf6c6
Chigozie-css <chigoziedivine62@gmail.com>
2024-04-11 11:24:17 -0700

    Figured out why checker was not happy with task 7 although I haven't see why it's not happy with task 4

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,6 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   const x = Number(process.argv[2]);
-  // Convert the command-line argument to a number
   let i = 0;
   while (i < x){
     console.log('C is fun');
