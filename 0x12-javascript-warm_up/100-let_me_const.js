f810dfc891caa0ca5eacd04ae866296beb38f564
Chigozie-css <chigoziedivine62@gmail.com>
2024-04-11 13:41:56 -0700

    Sorting errors

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -3,5 +3,4 @@
   This script assigns a value to a variable without explicit declaration.
   It's recommended to use 'var', 'let', or 'const' for variable declaration.
 */
-
-const myVar = 333;
+myVar = 333;
