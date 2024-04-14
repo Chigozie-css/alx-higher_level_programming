337e4f3d1107212e003b1ea3962e841cf8fd3f55
Chigozie-css <chigoziedivine62@gmail.com>
2024-04-14 12:17:53 -0700

    Sorting task 7 error

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,17 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """Create the engine to connect to the database"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    
-    """Create the tables based on the provided model"""
+
     Base.metadata.create_all(engine)
-    
-    """Create a sessionmaker to interact with the database"""
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    """Query the State table and print out the data"""
+
     for instance in session.query(State).order_by(State.id):
         print(instance.id, instance.name, sep=": ")
