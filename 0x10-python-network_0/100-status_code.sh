d2f2d0c928d5cbd13d95c2d99974bdd2af2e0b97
Chigozie-css <chigoziedivine62@gmail.com>
2024-04-25 11:32:00 -0700

    Advanced task 100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,4 @@
-#!/bash/bash
+#!/bin/bash
 # Sends a request to a URL passed as an argument and displays only the status code of the response.
 
-curl -s -o tmp_response.txt -w "%{http_code}" "$1"
-cat tmp_response.txt
-rm tmp_response.txt
+curl -s -o /dev/null -w "%{http_code}" "$1"
