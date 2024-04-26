a06e339afaa57f2efed312db14b1267ae1c27e7e
Chigozie-css <chigoziedivine62@gmail.com>
2024-04-26 06:54:17 -0700

    Sorting task 4 bugs

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Fetches the status of https://intranet.hbtn.io using the requests library."""
-
+"""Fetches status of https://alx-intranet.hbtn.io using requests."""
 import requests
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
