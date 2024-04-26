5f3df105b8722260b209b6ecfe80431e8d43fe7c
Chigozie-css <chigoziedivine62@gmail.com>
2024-04-26 07:08:20 -0700

    Updating task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,10 +5,7 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        q = ""
-    else:
-        q = sys.argv[1]
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
 
     response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
     try:
